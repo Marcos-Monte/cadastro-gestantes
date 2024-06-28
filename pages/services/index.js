b60979cdb7c108f692a3415d0ad6e0e04f2ef0b3
Burunga <monte_marcos@hotmail.com>
2024-06-28 20:48:30 -0300

    Erro de build ajustado: Condicional que para ver se 'listaGestante' é um array, se não for indica um erro

--- pages/services/index.js
+++ pages/services/index.js
@@ -3,25 +3,31 @@ import Registro from "../componentes/Registro";
 
 export default function mostrarGestantes(listaGestantes, erro){
 
+    console.log('Tipo de listaGestantes:', typeof listaGestantes);
+    console.log('listaGestantes:', listaGestantes);
+
+    if(!Array.isArray(listaGestantes)){
+        return <p>Dados inválidos: Não é um array</p>
+    }
+
     return (listaGestantes.length > 0 ?
     
-    listaGestantes.map(
+        listaGestantes.map(
 
-        (gestante, index) => (
-            
-            <Registro 
-                key={index}
-                nome={gestante.nome}
-                dn={gestante.dn}
-                endereco={gestante.endereco}
-                telefone={gestante.telefone}
-                equipe={gestante.equipe}
-            />
+            (gestante, index) => (
+                
+                <Registro 
+                    key={index}
+                    nome={gestante.nome}
+                    dn={gestante.dn}
+                    endereco={gestante.endereco}
+                    telefone={gestante.telefone}
+                    equipe={gestante.equipe}
+                />
 
-        )
-    ) :
-
-    <p>{erro}</p>)
+            )
+        ) : <p>{erro}</p>
+    )
 
 }
 
