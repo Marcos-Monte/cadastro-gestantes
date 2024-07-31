e96e1db9360db8ad81b76e0655c29134a08d3e28
Burunga <monte_marcos@hotmail.com>
2024-07-31 17:33:01 -0300

    feat: configurando instancia do 'axios' para fazer requisições HTTP no Endpoint: https://apicadastrogestantes.onrender.com
    fix: alterando nome dos arquivos: index para cadastro ; listas para index. O intuíto é que a aplicação abra diretamente na página com os registros e só clicando no link que seja enviada para o cadastro

--- pages/api/index.js
+++ pages/api/index.js
@@ -3,5 +3,5 @@ import axios from 'axios';
 
 // Configuranco uma Instancia de Axios. Para fazer requisições HTTP
 export const server = axios.create({
-    baseURL: 'http://localhost:8000'
+    baseURL: 'https://apicadastrogestantes.onrender.com'
 })
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,33 +1,120 @@
-import styles from "@/styles/Home.module.css";
-import Head from "next/head";
+// Importando conteúdo das dependencias
+import { useEffect, useState } from 'react';
 
+// Import de instancia de Axios: Biblioteca para fazer requisições HTTP
+import { server } from './api';
+
+// Importando arquivo de estilização
+import styles from "@/styles/Listas.module.css";
+
+//Importando componentes 
+import Head from "next/head";
+import Botao from "./componentes/BotaoSubmit";
 import Footer from "./componentes/Footer";
-import Formulario from "./componentes/Formulario";
 import Header from "./componentes/Header";
 
-export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>Gestantes</title>
-        <meta name="description" content="Pagina de cadastro de gestantes" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
-      <div className={`${styles.body}`}>
+// Importando funções da camada de serviços
+import mostrarGestante, { handleFiltro, handleSemFiltro } from '@/pages/services/service.jsx';
 
-        <Header />
+// Componente Principal
+export default function Listas() {
 
-        <main className={styles.main}>
+    // Variaveis de Estado
+    const [dados, setDados] = useState([]); // Estado para armazenar todos os dados
+    const [dadosFiltrados, setDadosFiltrados] = useState([]); // Estado para armazenar os dados filtrados
+    const [erro, setErro] = useState(null); // Estado para armazenar erros
 
-          <section className={`${styles.container} limit`}>
-            <Formulario />
-          </section>
-          
-        </main>
-        
-        <Footer />
+    // Função que faz a requisição HTTP GET e mostrando os dados
+    function buscarDados(){
 
-      </div>
-    </>
-  );
+        // Envia requisição 'GET' para o endpoint da API-backend com os dados do formulário
+        server.get('/listas')
+            .then((resposta) => {
+                setDados(resposta.data); // Dados armazenados na variável de estado
+                setDadosFiltrados(resposta.data); // Dados armazenados na variável de estado
+            })
+            .catch((erro) => {
+                setErro(erro.message || 'Erro ao carregar dados') // Dados armazenados na variável de estado
+            })
+    }
+
+    // useEffect, garante que a requisição seja feita apenas uma vez ao iniciar a pagina
+    useEffect(() => {
+
+        buscarDados()
+
+    }, [] // Array vazio garante que o useEffect rode apenas uma vez
+);
+
+    if (erro) return <div>Error: {erro}</div>;
+
+
+    return (
+        <>
+            <Head>
+                <title>Gestantes</title>
+                <meta name="description" content="Pagina de cadastro de gestantes" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <div className={`${styles.body}`}>
+
+                <Header 
+                    children={
+                        
+                        <div className={styles.containerFiltros}>
+                            <nav className={`${styles.filtros} limit`}>
+                                <Botao 
+                                    estilo='botaoSubmit' 
+                                    nome='Todas' 
+                                    funcao= {() => setDadosFiltrados(handleSemFiltro(dados))} 
+                                />
+                                <Botao 
+                                    estilo='botaoSubmit' 
+                                    nome='Azul' 
+                                    funcao={() => setDadosFiltrados(handleFiltro(dados, 'azul'))} 
+                                />
+                                <Botao 
+                                    estilo='botaoSubmit' 
+                                    nome='Verde' 
+                                    funcao={() => setDadosFiltrados(handleFiltro(dados, 'verde'))} 
+                                />
+                                <Botao 
+                                    estilo='botaoSubmit' 
+                                    nome='Amarelo' 
+                                    funcao={() => setDadosFiltrados(handleFiltro(dados, 'amarela'))} 
+                                />
+                                
+                            </nav>
+                        </div>
+                    }
+                />
+
+                <main className={styles.main}>
+
+                    <section className={`${styles.container} limit`}>
+
+                        <span className={styles.quantidade}>
+                            Quantidade : {dadosFiltrados.length}
+                        </span>
+                    
+                        <div className={styles.listas}>
+
+                                {
+                                    mostrarGestante(dadosFiltrados)
+                                    
+                                }
+
+                        </div>
+
+                    </section>
+                    
+                </main>
+
+                <Footer />
+
+            </div>
+        </>
+    )
 }
