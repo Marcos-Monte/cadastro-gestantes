5bfc626b9d1753d1204a183ad505b31c91e38c39
Burunga <monte_marcos@hotmail.com>
2024-07-31 19:34:52 -0300

    feat: criado arquivo '.env.production' que reconhece automaticamente que a aplicação está em produção e muda a variavel NEXT_PUBLIC_API_URL

--- pages/api/index.js
+++ pages/api/index.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 // Configuranco uma Instancia de Axios. Para fazer requisições HTTP
 export const server = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_URL,
+    baseURL: process.env.NEXT_PUBLIC_API_URL ,
     headers: {
         'Content-Type': 'application/json',
     }
